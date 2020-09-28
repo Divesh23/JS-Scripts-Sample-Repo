3a469a8d426b300e8a4bc8e43a999a1c5197f380
Divesh David <divesh.david@gmail.com>
2020-09-28 13:48:07 -0400

    Pending tests, hooks, only, skip

--- tests/controllers/test.js
+++ tests/controllers/test.js
@@ -1,19 +1,52 @@
+const { throws } = require('assert');
 var assert=require('assert');
+const authControllers = require('../../controllers/auth.controllers');
 var authController=require('../../controllers/auth.controllers')
 
 describe('AuthController',function(){
+    beforeEach (function settingValue(){
+        console.log('Running before each');
+        authController.setRoles(['user']);
 
-    describe('IsAuthorized',function(){
+    });
+
+    /*beforeEach ('This is an intentional abrupt error',function errorFunction(){
+       throws({error:'error'});
+
+    });*/
+
+    describe.only('IsAuthorized',function(){
 
         it('should return false if not authorized',function(){
-
-            assert.equal(false,authController.isAuthorized(['user'],'admin'));
+            authController.setRoles(['user']);
+            assert.equal(false,authController.isAuthorized('admin'));
 
         })
         it('should return true if  authorized',function(){
+            authController.setRoles(['user','admin']);
+            assert.equal(true,authController.isAuthorized('admin'));
 
-            assert.equal(true,authController.isAuthorized(['user','admin'],'admin'));
+        })
+        it('should not return get if not authorized');
+        it('should return get if authorized');
+    })
 
+    describe.only('IsAuthorizedAsync',function(){
+
+        it('should return false if not authorized',function(done){
+            // if (true){
+            //     //something environmental
+            //     this .skip()
+            // }
+            // else{
+            this.timeout(3000);
+            authController.setRoles(['user']);
+           authController.isAuthorizedAsync('admin',
+           function(isAuth){
+            assert.equal(false,isAuth);
+            done();
+           });
+        //}
         })
 
     })
